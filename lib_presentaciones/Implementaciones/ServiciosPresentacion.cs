8103c063045af09fed549d464ab01f63f4154fa4
Felipe <santiagomunozcordoba@gmail.com>
2024-11-26 09:11:36 -0500

    Interfaces y implementaciones en Servicios presentacion

---
+++ lib_presentaciones/Implementaciones/ServiciosPresentacion.cs
@@ -0,0 +1,109 @@
+﻿using lib_comunicaciones.Interfaces;
+using lib_entidades.Modelos;
+using lib_presentaciones.Interfaces;
+using lib_utilidades;
+
+namespace lib_presentaciones.Implementaciones
+{
+    public class ServiciosPresentacion : IServiciosPresentacion
+    {
+        private IServiciosComunicacion? iComunicacion = null;
+
+        public ServiciosPresentacion(IServiciosComunicacion iComunicacion)
+        {
+            this.iComunicacion = iComunicacion;
+        }
+
+        public async Task<List<Servicios>> Listar()
+        {
+            var lista = new List<Servicios>();
+            var datos = new Dictionary<string, object>();
+
+            var respuesta = await iComunicacion!.Listar(datos);
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            lista = JsonConversor.ConvertirAObjeto<List<Servicios>>(
+                JsonConversor.ConvertirAString(respuesta["Entidades"]));
+            return lista;
+        }
+
+        public async Task<List<Servicios>> Buscar(Servicios entidad, string tipo)
+        {
+            var lista = new List<Servicios>();
+            var datos = new Dictionary<string, object>();
+            datos["Entidad"] = entidad;
+            datos["Tipo"] = tipo;
+
+            var respuesta = await iComunicacion!.Buscar(datos);
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            lista = JsonConversor.ConvertirAObjeto<List<Servicios>>(
+                JsonConversor.ConvertirAString(respuesta["Entidades"]));
+            return lista;
+        }
+
+        public async Task<Servicios> Guardar(Servicios entidad)
+        {
+            if (entidad.Id != 0 || !entidad.Validar())
+            {
+                throw new Exception("lbFaltaInformacion");
+            }
+
+            var datos = new Dictionary<string, object>();
+            datos["Entidad"] = entidad;
+
+            var respuesta = await iComunicacion!.Guardar(datos);
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            entidad = JsonConversor.ConvertirAObjeto<Servicios>(
+                JsonConversor.ConvertirAString(respuesta["Entidad"]));
+            return entidad;
+        }
+
+        public async Task<Servicios> Modificar(Servicios entidad)
+        {
+            if (entidad.Id == 0 || !entidad.Validar())
+            {
+                throw new Exception("lbFaltaInformacion");
+            }
+
+            var datos = new Dictionary<string, object>();
+            datos["Entidad"] = entidad;
+
+            var respuesta = await iComunicacion!.Modificar(datos);
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            entidad = JsonConversor.ConvertirAObjeto<Servicios>(
+                JsonConversor.ConvertirAString(respuesta["Entidad"]));
+            return entidad;
+        }
+
+        public async Task<Servicios> Borrar(Servicios entidad)
+        {
+            if (entidad.Id == 0 || !entidad.Validar())
+            {
+                throw new Exception("lbFaltaInformacion");
+            }
+
+            var datos = new Dictionary<string, object>();
+            datos["Entidad"] = entidad;
+
+            var respuesta = await iComunicacion!.Borrar(datos);
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            entidad = JsonConversor.ConvertirAObjeto<Servicios>(
+                JsonConversor.ConvertirAString(respuesta["Entidad"]));
+            return entidad;
+        }
+    }
+}
---
+++ lib_presentaciones/Interfaces/IServiciosPresentacion.cs
@@ -0,0 +1,13 @@
+﻿using lib_entidades.Modelos;
+
+namespace lib_presentaciones.Interfaces
+{
+    public interface IServiciosPresentacion
+    {
+        Task<List<Servicios>> Listar();
+        Task<List<Servicios>> Buscar(Servicios entidad, string tipo);
+        Task<Servicios> Guardar(Servicios entidad);
+        Task<Servicios> Modificar(Servicios entidad);
+        Task<Servicios> Borrar(Servicios entidad);
+    }
+}
