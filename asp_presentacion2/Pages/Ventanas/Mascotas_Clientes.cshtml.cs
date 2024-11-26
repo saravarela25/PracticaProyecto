ba7875bcfd5128e15323b550f2b5c05b26bb85ec
Alejop28 <alejopineda19@hotmail.com>
2024-11-26 09:42:23 -0500

    Subiendo mascotas y mascotas_Clientes en lib_comunicaciones, lib_presentaciones y las pages en asp_presentacion

---
+++ asp_presentacion2/Pages/Ventanas/Mascotas_Clientes.cshtml.cs
@@ -0,0 +1,157 @@
+using lib_entidades.Modelos;
+using lib_presentaciones.Interfaces;
+using lib_utilidades;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace asp_presentacion.Pages.Ventanas
+{
+    public class Mascotas_ClientesModel : PageModel
+    {
+        private IMascotas_ClientesPresentacion? iPresentacion = null;
+
+        public Mascotas_ClientesModel(IMascotas_ClientesPresentacion iPresentacion)
+        {
+            try
+            {
+                this.iPresentacion = iPresentacion;
+                Filtro = new Mascotas_Clientes();
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        [BindProperty] public Enumerables.Ventanas Accion { get; set; }
+        [BindProperty] public Mascotas_Clientes? Actual { get; set; }
+        [BindProperty] public Mascotas_Clientes? Filtro { get; set; }
+        [BindProperty] public List<Mascotas_Clientes>? Lista { get; set; }
+
+        public virtual void OnGet() { OnPostBtRefrescar(); }
+
+        public void OnPostBtRefrescar()
+        {
+            try
+            {
+                // Filtro!.Cliente = Filtro!.Cliente ?? 0;
+                Filtro.Cliente = Filtro.Cliente != 0 ? Filtro.Cliente : 0; // revisar
+
+                 
+
+                Accion = Enumerables.Ventanas.Listas;
+                var task = this.iPresentacion!.Buscar(Filtro!, "Cliente");
+                task.Wait();
+                Lista = task.Result;
+                Actual = null;
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public virtual void OnPostBtNuevo()
+        {
+            try
+            {
+                Accion = Enumerables.Ventanas.Editar;
+                Actual = new Mascotas_Clientes();
+
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public virtual void OnPostBtModificar(string data)
+        {
+            try
+            {
+                OnPostBtRefrescar();
+                Accion = Enumerables.Ventanas.Editar;
+                Actual = Lista!.FirstOrDefault(x => x.Id.ToString() == data);
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public virtual void OnPostBtGuardar()
+        {
+            try
+            {
+                Accion = Enumerables.Ventanas.Editar;
+                Task<Mascotas_Clientes>? task = null;
+                if (Actual!.Id == 0)
+                    task = this.iPresentacion!.Guardar(Actual!);
+                else
+                    task = this.iPresentacion!.Modificar(Actual!);
+                task.Wait();
+                Actual = task.Result;
+                Accion = Enumerables.Ventanas.Listas;
+                OnPostBtRefrescar();
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public virtual void OnPostBtBorrarVal(string data)
+        {
+            try
+            {
+                OnPostBtRefrescar();
+                Accion = Enumerables.Ventanas.Borrar;
+                Actual = Lista!.FirstOrDefault(x => x.Id.ToString() == data);
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public virtual void OnPostBtBorrar()
+        {
+            try
+            {
+                var task = this.iPresentacion!.Borrar(Actual!);
+                Actual = task.Result;
+                OnPostBtRefrescar();
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public void OnPostBtCancelar()
+        {
+            try
+            {
+                Accion = Enumerables.Ventanas.Listas;
+                OnPostBtRefrescar();
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public void OnPostBtCerrar()
+        {
+            try
+            {
+                if (Accion == Enumerables.Ventanas.Listas)
+                    OnPostBtRefrescar();
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+    }
+}
---
+++ lib_presentaciones/Interfaces/IMascotas_ClientesPresentacion.cs
@@ -0,0 +1,13 @@
+﻿using lib_entidades.Modelos;
+
+namespace lib_presentaciones.Interfaces
+{
+    public interface IMascotas_ClientesPresentacion
+    {
+        Task<List<Mascotas_Clientes>> Listar();
+        Task<List<Mascotas_Clientes>> Buscar(Mascotas_Clientes entidad, string tipo);
+        Task<Mascotas_Clientes> Guardar(Mascotas_Clientes entidad);
+        Task<Mascotas_Clientes> Modificar(Mascotas_Clientes entidad);
+        Task<Mascotas_Clientes> Borrar(Mascotas_Clientes entidad);
+    }
+}
