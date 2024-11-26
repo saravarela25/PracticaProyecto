40621e7a38387475f5425b47e6240593afe3ab05
daniel cano <krlag2011@gmail.com>
2024-11-26 09:04:12 -0500

    agregue presentaciones de Detalles_Facturas comunicaciones y asp_presentacion

--- lib_presentaciones/Interfaces/IDetalles_FacturasPresentacion.cs
+++ lib_presentaciones/Interfaces/IDetalles_FacturasPresentacion.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using lib_entidades.Modelos;
 
 namespace lib_presentaciones.Interfaces
 {
-    internal interface IDetalles_FacturasPresentacion
+    public interface IDetalles_FacturasPresentacion
     {
+        Task<List<Detalles_Facturas>> Listar();
+        Task<List<Detalles_Facturas>> Buscar(Detalles_Facturas entidad, string tipo);
+        Task<Detalles_Facturas> Guardar(Detalles_Facturas entidad);
+        Task<Detalles_Facturas> Modificar(Detalles_Facturas entidad);
+        Task<Detalles_Facturas> Borrar(Detalles_Facturas entidad);
     }
 }
