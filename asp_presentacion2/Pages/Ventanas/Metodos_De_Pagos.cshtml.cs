62bcf72e6ca54c50649c5bb0a379bf4c6aca2d4f
Alejo Pineda <alejopineda19@hotmail.com>
2024-11-26 23:05:54 -0500

    Montando metodos de pago en toda la capa de presentacion

---
+++ asp_presentacion2/Pages/Ventanas/Metodos_De_Pagos.cshtml.cs
@@ -0,0 +1,154 @@
+using lib_entidades.Modelos;
+using lib_presentaciones.Interfaces;
+using lib_utilidades;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace asp_presentacion.Pages.Ventanas
+{
+    public class Metodos_De_PagosModel : PageModel
+    {
+        private IMetodos_De_PagosPresentacion? iPresentacion = null;
+
+        public Metodos_De_PagosModel(IMetodos_De_PagosPresentacion iPresentacion)
+        {
+            try
+            {
+                this.iPresentacion = iPresentacion;
+                Filtro = new Metodos_De_Pagos();
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        [BindProperty] public Enumerables.Ventanas Accion { get; set; }
+        [BindProperty] public Metodos_De_Pagos? Actual { get; set; }
+        [BindProperty] public Metodos_De_Pagos? Filtro { get; set; }
+        [BindProperty] public List<Metodos_De_Pagos>? Lista { get; set; }
+
+        public virtual void OnGet() { OnPostBtRefrescar(); }
+
+        public void OnPostBtRefrescar()
+        {
+            try
+            {
+                Filtro!.Tipo_Metodo_Pago = Filtro!.Tipo_Metodo_Pago ?? "";
+
+                Accion = Enumerables.Ventanas.Listas;
+                var task = this.iPresentacion!.Buscar(Filtro!, "NOMBRE");
+                task.Wait();
+                Lista = task.Result;
+                Actual = null;
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public virtual void OnPostBtNuevo()
+        {
+            try
+            {
+                Accion = Enumerables.Ventanas.Editar;
+                Actual = new Metodos_De_Pagos();
+
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public virtual void OnPostBtModificar(string data)
+        {
+            try
+            {
+                OnPostBtRefrescar();
+                Accion = Enumerables.Ventanas.Editar;
+                Actual = Lista!.FirstOrDefault(x => x.Id.ToString() == data);
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public virtual void OnPostBtGuardar()
+        {
+            try
+            {
+                Accion = Enumerables.Ventanas.Editar;
+                Task<Metodos_De_Pagos>? task = null;
+                if (Actual!.Id == 0)
+                    task = this.iPresentacion!.Guardar(Actual!);
+                else
+                    task = this.iPresentacion!.Modificar(Actual!);
+                task.Wait();
+                Actual = task.Result;
+                Accion = Enumerables.Ventanas.Listas;
+                OnPostBtRefrescar();
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public virtual void OnPostBtBorrarVal(string data)
+        {
+            try
+            {
+                OnPostBtRefrescar();
+                Accion = Enumerables.Ventanas.Borrar;
+                Actual = Lista!.FirstOrDefault(x => x.Id.ToString() == data);
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public virtual void OnPostBtBorrar()
+        {
+            try
+            {
+                var task = this.iPresentacion!.Borrar(Actual!);
+                Actual = task.Result;
+                OnPostBtRefrescar();
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public void OnPostBtCancelar()
+        {
+            try
+            {
+                Accion = Enumerables.Ventanas.Listas;
+                OnPostBtRefrescar();
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public void OnPostBtCerrar()
+        {
+            try
+            {
+                if (Accion == Enumerables.Ventanas.Listas)
+                    OnPostBtRefrescar();
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+    }
+}
---
+++ lib_presentaciones/Implementaciones/Metodos_De_PagosPresentacion.cs
@@ -0,0 +1,109 @@
+﻿using lib_comunicaciones.Interfaces;
+using lib_entidades.Modelos;
+using lib_presentaciones.Interfaces;
+using lib_utilidades;
+
+namespace lib_presentaciones.Implementaciones
+{
+    public class Metodos_De_PagosPresentacion : IMetodos_De_PagosPresentacion
+    {
+        private IMetodos_De_PagosComunicacion? iComunicacion = null;
+
+        public Metodos_De_PagosPresentacion(IMetodos_De_PagosComunicacion iComunicacion)
+        {
+            this.iComunicacion = iComunicacion;
+        }
+
+        public async Task<List<Metodos_De_Pagos>> Listar()
+        {
+            var lista = new List<Metodos_De_Pagos>();
+            var datos = new Dictionary<string, object>();
+
+            var respuesta = await iComunicacion!.Listar(datos);
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            lista = JsonConversor.ConvertirAObjeto<List<Metodos_De_Pagos>>(
+                JsonConversor.ConvertirAString(respuesta["Entidades"]));
+            return lista;
+        }
+
+        public async Task<List<Metodos_De_Pagos>> Buscar(Metodos_De_Pagos entidad, string tipo)
+        {
+            var lista = new List<Metodos_De_Pagos>();
+            var datos = new Dictionary<string, object>();
+            datos["Entidad"] = entidad;
+            datos["Tipo"] = tipo;
+
+            var respuesta = await iComunicacion!.Buscar(datos);
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            lista = JsonConversor.ConvertirAObjeto<List<Metodos_De_Pagos>>(
+                JsonConversor.ConvertirAString(respuesta["Entidades"]));
+            return lista;
+        }
+
+        public async Task<Metodos_De_Pagos> Guardar(Metodos_De_Pagos entidad)
+        {
+            if (entidad.Id != 0 || !entidad.Validar())
+            {
+                throw new Exception("lbFaltaInformacion");
+            }
+
+            var datos = new Dictionary<string, object>();
+            datos["Entidad"] = entidad;
+
+            var respuesta = await iComunicacion!.Guardar(datos);
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            entidad = JsonConversor.ConvertirAObjeto<Metodos_De_Pagos>(
+                JsonConversor.ConvertirAString(respuesta["Entidad"]));
+            return entidad;
+        }
+
+        public async Task<Metodos_De_Pagos> Modificar(Metodos_De_Pagos entidad)
+        {
+            if (entidad.Id == 0 || !entidad.Validar())
+            {
+                throw new Exception("lbFaltaInformacion");
+            }
+
+            var datos = new Dictionary<string, object>();
+            datos["Entidad"] = entidad;
+
+            var respuesta = await iComunicacion!.Modificar(datos);
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            entidad = JsonConversor.ConvertirAObjeto<Metodos_De_Pagos>(
+                JsonConversor.ConvertirAString(respuesta["Entidad"]));
+            return entidad;
+        }
+
+        public async Task<Metodos_De_Pagos> Borrar(Metodos_De_Pagos entidad)
+        {
+            if (entidad.Id == 0 || !entidad.Validar())
+            {
+                throw new Exception("lbFaltaInformacion");
+            }
+
+            var datos = new Dictionary<string, object>();
+            datos["Entidad"] = entidad;
+
+            var respuesta = await iComunicacion!.Borrar(datos);
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            entidad = JsonConversor.ConvertirAObjeto<Metodos_De_Pagos>(
+                JsonConversor.ConvertirAString(respuesta["Entidad"]));
+            return entidad;
+        }
+    }
+}
