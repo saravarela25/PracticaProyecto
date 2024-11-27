e39ac19d2211cca6536ea5c8c5a9477851e40f0e
SebasCiro <jimenez.ciro.sebastian01@gmail.com>
2024-11-27 11:15:10 -0500

    Usuarios y Roles, capa de aplicaciones

--- lib_entidades/Modelos/Roles.cs
+++ lib_entidades/Modelos/Roles.cs
@@ -1,8 +1,17 @@
-﻿namespace lib_entidades.Modelos
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace lib_entidades.Modelos
 {
     public class Roles
     {
         public int Id { get; set; }
         public string? Nombre { get; set; }
+        public bool Validar()
+        {
+            if (string.IsNullOrEmpty(Nombre))
+                return false;
+
+            return true;
+        }
     }
 }
