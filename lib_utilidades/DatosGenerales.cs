566f8af36d358527ffc64797d9fbea7d06960f82
Alejo Pineda <alejopineda19@hotmail.com>
2024-11-28 01:16:49 -0500

    Ya funciona el Metodos_De_Pagos

--- lib_utilidades/DatosGenerales.cs
+++ lib_utilidades/DatosGenerales.cs
@@ -2,7 +2,7 @@
 {
     public class DatosGenerales
     {
-        public static string ruta_json = @"C:\Users\123\source\repos\PracticaProyecto\secrets.abc";
+        public static string ruta_json = @"C:\Users\alejo\source\repos\PracticaProyecto\secrets.abc";
         public static bool usa_azure = false;
         public static string clave = "EVBgi345936456ghhVBJGtgnifytsidi3456678jhgUTytutyiiyi";
         public static string usuario_datos = EncriptarConversor.Encriptar("Test.Trghhjsgdj");
