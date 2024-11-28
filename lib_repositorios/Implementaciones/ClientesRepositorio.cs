5d319bc0a845d893deca568b1dd836a054e97142
saravarela25 <saravarelagarcia25@gmail.com>
2024-11-27 20:38:56 -0500

    Cambios en CLientes repositorio, debido a problemas con auditorias

--- lib_repositorios/Implementaciones/ClientesRepositorio.cs
+++ lib_repositorios/Implementaciones/ClientesRepositorio.cs
@@ -7,12 +7,10 @@ namespace lib_repositorios.Implementaciones
     public class ClientesRepositorio : IClientesRepositorio
     {
         private Conexion? conexion = null;
-        private AuditoriasRepositorio? auditoria = null;
 
-        public ClientesRepositorio(Conexion conexion, AuditoriasRepositorio? auditoria)
+        public ClientesRepositorio(Conexion conexion)
         {
             this.conexion = conexion;
-            this.auditoria = auditoria;
         }
 
         public void Configurar(string string_conexion)
@@ -22,34 +20,16 @@ namespace lib_repositorios.Implementaciones
 
         public List<Clientes> Listar()
         {
-            auditoria!.Guardar(
-                "Usuario - Prueba",
-                "Clientes",
-                "Select * from Clientes",
-                "Se listaron todos los Clientes."
-            );
             return conexion!.Listar<Clientes>();
         }
         public List<Clientes> Buscar(Expression<Func<Clientes, bool>> condiciones)
         {
-            auditoria!.Guardar(
-                "Usuario - Prueba",
-                "Clientes",
-                "Select",
-                $"Se lista en base a las condiciones: {condiciones}"
-            );
             return conexion!.Buscar(condiciones);
         }
         public Clientes Guardar(Clientes entidad)
         {
             conexion!.Guardar(entidad);
             conexion!.GuardarCambios();
-            auditoria!.Guardar(
-                "Usuario - Prueba",
-                "Clientes",
-                "Insert",
-                $"Se agregó un nuevo cliente: {entidad.Nombre} (Id: {entidad.Id})"
-            );
             return entidad;
         }
 
@@ -57,12 +37,6 @@ namespace lib_repositorios.Implementaciones
         {
             conexion!.Modificar(entidad);
             conexion!.GuardarCambios();
-            auditoria!.Guardar(
-                "Usuario - Prueba",
-                "Clientes",
-                "Update",
-                $"Se actualizó el cliente con Id: {entidad.Id}"
-            );
             return entidad;
         }
 
@@ -70,12 +44,6 @@ namespace lib_repositorios.Implementaciones
         {
             conexion!.Borrar(entidad);
             conexion!.GuardarCambios();
-            auditoria!.Guardar(
-                "Usuario - Prueba",
-                "Clientes",
-                "Delete",
-                $"Se eliminó la mascota con Id: {entidad.Id}"
-            );
             return entidad;
         }
     }
